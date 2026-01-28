8ba0c7e5b75b4873e6f73fdf0a90780244028c7f
alex <sanagurbo@gmail.com>
2026-01-29 01:06:14 +0300

    Initial commit: Pinsk app with slider and maps

--- app/src/main/java/com/example/pinsk/PanoramaActivity.kt
+++ app/src/main/java/com/example/pinsk/PanoramaActivity.kt
@@ -1,7 +1,10 @@
 package com.example.pinsk
 
 import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
@@ -12,24 +15,23 @@ class PanoramaActivity : AppCompatActivity() {
 
         val webView = findViewById<WebView>(R.id.webViewPanorama)
 
-        webView.webChromeClient = android.webkit.WebChromeClient()
-        webView.webViewClient = android.webkit.WebViewClient()
+        webView.webChromeClient = WebChromeClient()
+        webView.webViewClient = WebViewClient()
 
         webView.settings.apply {
             javaScriptEnabled = true
             allowFileAccess = true
             allowContentAccess = true
             domStorageEnabled = true
-
             allowFileAccessFromFileURLs = true
             allowUniversalAccessFromFileURLs = true
-
-            databaseEnabled = true
-            mixedContentMode = android.webkit.WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+            mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
         }
 
         webView.loadUrl("file:///android_asset/tour.html")
 
-        findViewById<Button>(R.id.btnExitPano).setOnClickListener { finish() }
+        findViewById<Button>(R.id.btnExitPano).setOnClickListener {
+            finish()
+        }
     }
 }
